7f0f179fbd42d4c4da17c52b97b0c2e44b20d059
iaifrns <nsinifranc@gmail.com>
2025-02-18 09:33:07 +0100

    otp page started

--- app/(auth)/otp-validation.tsx
+++ app/(auth)/otp-validation.tsx
@@ -1,15 +1,24 @@
-import React, { Component } from "react";
+import React, { useRef } from "react";
 import { ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import OTPTextInput from "react-native-otp-textinput";
 
 const OtpValidation = () => {
+  const otpRef = useRef(null);
+
   return (
     <SafeAreaView className="h-full w-full">
-        <ScrollView className="h-full w-full">
-            <View className="w-full h-full justify-center items-center">
-                <Text>otp validation screen</Text>
-            </View>
-        </ScrollView>
+      <ScrollView className="h-full w-full">
+        <View className="w-full h-full justify-center items-center">
+          <OTPTextInput
+            ref={otpRef}
+            inputCount={6}
+            keyboardType="numeric"
+            autoFocus={true}
+          />
+          <Text>otp validation screen</Text>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
